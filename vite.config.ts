d41788cc6599ac50eb28a15a25d27fcb2086c969
OliverHeffernan <oliverheffernan@icloud.com>
2025-11-11 12:05:14 +1300

    updated basedir to work with olihef.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/paperWeight/dist/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/paperWeight/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
